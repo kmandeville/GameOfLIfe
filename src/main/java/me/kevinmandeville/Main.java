52ea74ebcab925449ccae309507fbffe5dedc122
Kevin Mandeville <kmandeville@techtarget.com>
2023-10-11 14:53:40 -0400

    Removed comments from Main class

--- src/main/java/me/kevinmandeville/Main.java
+++ src/main/java/me/kevinmandeville/Main.java
@@ -27,22 +27,6 @@ public class Main {
 
         Game game = new Game(boardSize, pauseLength, percentOfCellsToStart);
         game.start();
-
-        // choose number of alive cells to start with (must be smaller than x % of the total number of spaces available.
-        // Total spaces available is x * y
-
-        // Randomly populate grid with alive cells based on number above
-        // use Random number generator twice, once between 0 -> x.max and again for 0 -> y.max
-
-        // game loop
-        // look through game board and determine based on rules which cells suddenly become alive and which ones die
-        // cell is "born"" if it has EXACTLY 3 neighbors, cell stays alive as long as it has 2 or 3 neighbors, dies otherwise
-
-        // change cells
-        // print board
-        // check end game conditions (no cells changed, all cells are dead, check if pattern flips back and forth (how?))
-        // pause based on how long each turn should take
-        // end loop
     }
 
 
